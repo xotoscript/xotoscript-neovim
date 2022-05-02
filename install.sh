f5857b80300d47f027986bf3c471476939cf6927
xotosphere <xotosphere@gmail.com>
2022-05-02 17:28:42 +0200

    added install

--- install.sh
+++ install.sh
@@ -1,21 +1,41 @@
 USER_HOME=$HOME
-
+BYPASS=true
 VERSION=0.6.0
 
+################################################ EDITOR
+
+# CHECK VERSION
 if [[ "$1" == "" ]]; then
-  NEOVIM_VERSION=VERSION
+	NEOVIM_VERSION=VERSION
 else
- NEOVIM_VERSION=$1
+	NEOVIM_VERSION=$1
+fi
+
+# CHECK BYPASS
+if [[ "$2" == "" ]]; then
+	MAIN_BYPASS=BYPASS
+else
+	NEOVIM_VERSION=$1
 fi
 
 ################################################ EDITOR
 
-set -ex && sudo apt-get install --yes --no-install-recommends --allow-unauthenticated \
-	tmux \
-	vim
+function install() {
+	sudo curl -LO https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim-linux64.tar.gz ${USER_HOME}/nvim-linux64.tar.gz | bash
+	tar xzvf ${USER_HOME}/nvim-linux64.tar.gz
+	sudo ln -sf ${USER_HOME}/nvim-linux64/bin/nvim /usr/bin/nvim
+}
 
-sudo curl -LO https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim-linux64.tar.gz ${USER_HOME}/nvim-linux64.tar.gz | bash
-tar xzvf ${USER_HOME}/nvim-linux64.tar.gz
-sudo ln -sf ${USER_HOME}/nvim-linux64/bin/nvim /usr/bin/nvim
+################################################ EDITOR
+
+if [ $(which vim 2>/dev/null) || MAIN_BYPASS = true ]; then
+	echo "vim found"
+	install
+else
+	echo "vim is not found please install it "
+
+fi
+
+echo "complete : "
 
 ################################################
