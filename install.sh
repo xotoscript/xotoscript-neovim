ad750c7e21ea923e6c6edae08c06dc0cce0f4e85
xotosphere <xotosphere@gmail.com>
2022-05-02 17:52:15 +0200

    added changes

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 USER_HOME=$HOME
-
 VERSION=0.6.0
 
 ################################################ EDITOR
+
+# Detect the platform (similar to $OSTYPE)
+OS="$(uname)"
+case $OS in
+'Linux')
+	OS='nvim-linux64.tar.gz'
+	;;
+'Darwin')
+	OS='nvim-macos.tar.gz'
+	;;
+*) ;;
+esac
 
 # CHECK VERSION
 if [[ "$1" == "" ]]; then
@@ -14,14 +25,16 @@ fi
 ################################################ EDITOR
 
 function install() {
-	sudo curl -LO https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim-linux64.tar.gz ${USER_HOME}/nvim-linux64.tar.gz | bash
+	echo "☑ installing from : "
+	echo "https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim-linux64.tar.gz ${USER_HOME}/${OS}"
+	sudo curl -LO https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim-linux64.tar.gz ${USER_HOME}/${OS} | bash
 	tar xzvf ${USER_HOME}/nvim-linux64.tar.gz
 	sudo ln -sf ${USER_HOME}/nvim-linux64/bin/nvim /usr/bin/nvim
 }
 
 ################################################ EDITOR
 
-	install
+install
 
 echo "complete : 🍳"
 
