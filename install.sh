e44f529864c9ea77cf8b6ba6a6532ec9f83763fc
xotosphere <xotosphere@gmail.com>
2022-05-02 18:16:06 +0200

    added url changes

--- install.sh
+++ install.sh
@@ -24,10 +24,11 @@ fi
 ################################################ EDITOR
 
 function install() {
+	URL="https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/${OS}"
 	echo "☑ installing from : "
-	echo "https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/$OS ${USER_HOME}/$OS"
+	echo "$URL ${USER_HOME}/$OS"
 	echo ""
-	sudo curl -LO https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/$OS ${USER_HOME}/$OS 
+	sudo curl -LO $URL ${USER_HOME}/$OS
 	tar xzvf ${USER_HOME}/$OS
 	sudo ln -sf ${USER_HOME}/$OS/bin/nvim /usr/local/bin/nvim
 }
