eaed6e930b08785dfd74ca1cf051173e6aad681d
xotosphere <xotosphere@gmail.com>
2022-05-03 21:23:57 +0200

    added installation

--- install.sh
+++ install.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
-################################# COLOR ENVIRONMENTS
+######################################### ENVIRONMENTS
 
 NEOVIM_VERSION=0.7.0
 
 NC='\033[0m'
-BLACK='\033[0;30m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
+BLACK='\033[0;30m'  # Black
+RED='\033[0;31m'    # Red
+GREEN='\033[0;32m'  # Green
+YELLOW='\033[0;33m' # Yellow
+BLUE='\033[0;34m'   # Blue
+PURPLE='\033[0;35m' # Purple
+CYAN='\033[0;36m'   # Cyan
+WHITE='\033[0;37m'  # White
 
-################################# VARS
+################################# CLEANING
 
 SYSTEM_MACHINE=''
 SYSTEM_OS="$(uname)"
-
-################################# CLEAN
 
 trap cleanup 1 2 3 6
 PROXY_ID=''
@@ -30,68 +28,7 @@ cleanup() {
 	exit 1
 }
 
-##################### PROGRESSBAR
-
-progreSh() {
-	echo ""
-	echo ""
-	LR='\033[1;31m'
-	LG='\033[1;32m'
-	LY='\033[1;33m'
-	LC='\033[1;36m'
-	LW='\033[1;37m'
-	NC='\033[0m'
-	if [ "${1}" = "0" ]; then TME=$(date +"%s"); fi
-	SEC=$(printf "%04d\n" $(($(date +"%s") - ${TME})))
-	SEC="$SEC sec"
-	PRC=$(printf "%.0f" ${1})
-	SHW=$(printf "%3d\n" ${PRC})
-	LNE=$(printf "%.0f" $((${PRC} / 2)))
-	LRR=$(printf "%.0f" $((${PRC} / 2 - 12)))
-	if [ ${LRR} -le 0 ]; then LRR=0; fi
-	LYY=$(printf "%.0f" $((${PRC} / 2 - 24)))
-	if [ ${LYY} -le 0 ]; then LYY=0; fi
-	LCC=$(printf "%.0f" $((${PRC} / 2 - 36)))
-	if [ ${LCC} -le 0 ]; then LCC=0; fi
-	LGG=$(printf "%.0f" $((${PRC} / 2 - 48)))
-	if [ ${LGG} -le 0 ]; then LGG=0; fi
-	LRR_=""
-	LYY_=""
-	LCC_=""
-	LGG_=""
-	for ((i = 1; i <= 13; i++)); do
-		DOTS=""
-		for ((ii = ${i}; ii < 13; ii++)); do DOTS="${DOTS}."; done
-		if [ ${i} -le ${LNE} ]; then LRR_="${LRR_}#"; else LRR_="${LRR_}."; fi
-		echo -ne "${2} ${LW}${SEC}  ${LR}${LRR_}${DOTS}${LY}............${LC}............${LG}............ ${SHW}%${NC}\r"
-		if [ ${LNE} -ge 1 ]; then sleep .05; fi
-	done
-	for ((i = 14; i <= 25; i++)); do
-		DOTS=""
-		for ((ii = ${i}; ii < 25; ii++)); do DOTS="${DOTS}."; done
-		if [ ${i} -le ${LNE} ]; then LYY_="${LYY_}#"; else LYY_="${LYY_}."; fi
-		echo -ne "${2} ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${DOTS}${LC}............${LG}............ ${SHW}%${NC}\r"
-		if [ ${LNE} -ge 14 ]; then sleep .05; fi
-	done
-	for ((i = 26; i <= 37; i++)); do
-		DOTS=""
-		for ((ii = ${i}; ii < 37; ii++)); do DOTS="${DOTS}."; done
-		if [ ${i} -le ${LNE} ]; then LCC_="${LCC_}#"; else LCC_="${LCC_}."; fi
-		echo -ne "${2} ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${DOTS}${LG}............ ${SHW}%${NC}\r"
-		if [ ${LNE} -ge 26 ]; then sleep .05; fi
-	done
-	for ((i = 38; i <= 49; i++)); do
-		DOTS=""
-		for ((ii = ${i}; ii < 49; ii++)); do DOTS="${DOTS}."; done
-		if [ ${i} -le ${LNE} ]; then LGG_="${LGG_}#"; else LGG_="${LGG_}."; fi
-		echo -ne "${2} ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${LG}${LGG_}${DOTS} ${SHW}%${NC}\r"
-		if [ ${LNE} -ge 38 ]; then sleep .05; fi
-	done
-	echo ""
-	echo ""
-}
-
-################################# CASES
+################################################ CASES
 
 case $SYSTEM_OS in
 'Linux')
@@ -105,10 +42,15 @@ case $SYSTEM_OS in
 *) ;;
 esac
 
-################################# INSTALL
+################################################ INSTALL
 
-function installNvim() {
+function install() {
+	echo ""
+	echo "${GREEN} 🧑🏽‍💻 INSTALLING NVIM${NC}"
+	echo ""
 	URL="https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/${SYSTEM_MACHINE}.tar.gz"
+	echo $URL
+	echo ""
 	curl -LO $URL
 	tar xzvf ${SYSTEM_MACHINE}.tar.gz >/dev/null 2>&1
 	rm -rf ${SYSTEM_MACHINE}.tar.gz
@@ -116,54 +58,69 @@ function installNvim() {
 	ln -sf ${HOME}/${FILE}/bin/nvim /usr/local/bin/nvim
 }
 
-################################# REMOVE NVIM
+################################################ REMOVE NVIM
 
 function removeInstalledNvim() {
+	echo ""
+	echo "${RED} ❌ REMOVING NVIM${NC}"
+	echo ""
 	rm -rf ${HOME}/nvim-osx64 ${HOME}/nvim.appimage /usr/local/Cellar/nvim /usr/local/bin/nvim ${HOME}/.cache/nvim ${HOME}/.cache/nvim ${HOME}/.local/share/nvim /usr/local/share/lua /usr/local/Cellar/luajit-openresty /usr/local/share/luajit-2.1.0-beta3 /usr/local/lib/lua
 }
 
-################################# REMOVE LVIM
+################################################ REMOVE LVIM
 
 function removeInstalledLvim() {
+	echo ""
+	echo "${RED} ❌ REMOVING LVIM${NC}"
+	echo ""
 	bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/uninstall.sh)
 }
 
-################################# CREATE NVIM FOLDER
+################################################ CREATE NVIM FOLDER
 
-function createNvimDir() {
+function createNvim() {
 	if [ ! -d "${HOME}/.config" ]; then
+		echo ""
+		echo "${RED} ❌ NO ./.CONFIG CREATING${NC}"
+		echo ""
 		mkdir ${HOME}/.config
 	fi
 }
 
-################################# CREATE EDITOR
+################################################ CREATE EDITOR
 
 function createEditor() {
 	if [ -d "${HOME}/.config/nvim" ]; then
+		echo ""
+		echo "${RED} ❌ REMOVING NVIM FROM ./.CONFIG FOLDER${NC}"
+		echo ""
 		rm -rf ${HOME}/.config/nvim
+		echo ""
+		echo "${GREEN} 🧑🏽‍💻 INSTALLING LVIM${NC}"
+		echo ""
 		bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
+		echo ""
+		echo "${GREEN} 🧑🏽‍💻 INSTALLING COSMIC-NVIM${NC}"
+		echo ""
 		git clone git@github.com:CosmicNvim/CosmicNvim.git ${HOME}/.config/nvim
 	else
+		echo ""
 		bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
 		git clone git@github.com:CosmicNvim/CosmicNvim.git ${HOME}/.config/nvim
 	fi
 }
 
-################################# PROCESS
+################################################ PROCESS
 
-progreSh 10 "${RED} ❌ REMOVING NVIM${NC}"
 removeInstalledNvim
-progreSh 20 "${RED} ❌ REMOVING LVIM{NC}"
 removeInstalledLvim
-progreSh 40 "${GREEN} 🧑🏽‍💻 DOWNLOADING NEOVIM${NC}"
-installNvim
-progreSh 60 "${GREEN} 🧑🏽‍💻 SETTING UP NVIM DIR ${NC}"
-createNvimDir
-progreSh 80 "${GREEN} 🧑🏽‍💻 INSTALLING COSMICVIM AND LVIM ${NC}"
+install
+createNvim
 createEditor
-progreSh 100 "${PURPLE} 🖍 CLEANING UP ${NC}"
 
-################################# END
+echo ""
+
+################################################ END
 
 echo "${YELLOW}.########.####.##....##.####..######..##.....##.########.########.${NC}"
 echo "${YELLOW}.##........##..###...##..##..##....##.##.....##.##.......##.....##${NC}"
@@ -175,11 +132,11 @@ echo "${YELLOW}.##.......####.##....##.####..######..##.....##.########.########
 
 echo ""
 echo ""
-echo "${YELLOW} NOW JUST RUN : ${NC}"
+echo "${YELLOW} 🖍 NOW JUST RUN : ${NC}"
 echo ""
 echo "${GREEN}nvim +PackerSync # to install and run all deps for nvim ${NC}"
 echo "${GREEN}lvim +PackerSync # to install and run all deps for lvim ${NC}"
 echo ""
 echo ""
 
-################################# END
+################################################ END
