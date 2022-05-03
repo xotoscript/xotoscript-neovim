96796a4965b41cd6105e2cb1939b4f401e36a9d3
xotosphere <xotosphere@gmail.com>
2022-05-03 21:22:02 +0200

    added small changes

--- install.sh
+++ install.sh
@@ -33,6 +33,8 @@ cleanup() {
 ##################### PROGRESSBAR
 
 progreSh() {
+	echo ""
+	echo ""
 	LR='\033[1;31m'
 	LG='\033[1;32m'
 	LY='\033[1;33m'
@@ -61,38 +63,32 @@ progreSh() {
 		DOTS=""
 		for ((ii = ${i}; ii < 13; ii++)); do DOTS="${DOTS}."; done
 		if [ ${i} -le ${LNE} ]; then LRR_="${LRR_}#"; else LRR_="${LRR_}."; fi
-		echo ""
 		echo -ne "${2} ${LW}${SEC}  ${LR}${LRR_}${DOTS}${LY}............${LC}............${LG}............ ${SHW}%${NC}\r"
-		echo ""
 		if [ ${LNE} -ge 1 ]; then sleep .05; fi
 	done
 	for ((i = 14; i <= 25; i++)); do
 		DOTS=""
 		for ((ii = ${i}; ii < 25; ii++)); do DOTS="${DOTS}."; done
 		if [ ${i} -le ${LNE} ]; then LYY_="${LYY_}#"; else LYY_="${LYY_}."; fi
-		echo ""
 		echo -ne "${2} ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${DOTS}${LC}............${LG}............ ${SHW}%${NC}\r"
-		echo ""
 		if [ ${LNE} -ge 14 ]; then sleep .05; fi
 	done
 	for ((i = 26; i <= 37; i++)); do
 		DOTS=""
 		for ((ii = ${i}; ii < 37; ii++)); do DOTS="${DOTS}."; done
 		if [ ${i} -le ${LNE} ]; then LCC_="${LCC_}#"; else LCC_="${LCC_}."; fi
-		echo ""
 		echo -ne "${2} ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${DOTS}${LG}............ ${SHW}%${NC}\r"
-		echo ""
 		if [ ${LNE} -ge 26 ]; then sleep .05; fi
 	done
 	for ((i = 38; i <= 49; i++)); do
 		DOTS=""
 		for ((ii = ${i}; ii < 49; ii++)); do DOTS="${DOTS}."; done
 		if [ ${i} -le ${LNE} ]; then LGG_="${LGG_}#"; else LGG_="${LGG_}."; fi
-		echo ""
 		echo -ne "${2} ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${LG}${LGG_}${DOTS} ${SHW}%${NC}\r"
-		echo ""
 		if [ ${LNE} -ge 38 ]; then sleep .05; fi
 	done
+	echo ""
+	echo ""
 }
 
 ################################# CASES
@@ -155,23 +151,17 @@ function createEditor() {
 
 ################################# PROCESS
 
-progreSh 0 "${RED} ❌ REMOVING NVIM${NC}"
-echo ""
+progreSh 10 "${RED} ❌ REMOVING NVIM${NC}"
 removeInstalledNvim
 progreSh 20 "${RED} ❌ REMOVING LVIM{NC}"
-echo ""
 removeInstalledLvim
 progreSh 40 "${GREEN} 🧑🏽‍💻 DOWNLOADING NEOVIM${NC}"
-echo ""
 installNvim
 progreSh 60 "${GREEN} 🧑🏽‍💻 SETTING UP NVIM DIR ${NC}"
-echo ""
 createNvimDir
 progreSh 80 "${GREEN} 🧑🏽‍💻 INSTALLING COSMICVIM AND LVIM ${NC}"
-echo ""
 createEditor
 progreSh 100 "${PURPLE} 🖍 CLEANING UP ${NC}"
-echo ""
 
 ################################# END
 
