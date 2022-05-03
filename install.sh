4d7219eaacbe1c03a4e5559efa185e979e3eae0b
xotosphere <xotosphere@gmail.com>
2022-05-03 20:59:56 +0200

    added progress

--- install.sh
+++ install.sh
@@ -32,61 +32,27 @@ cleanup() {
 
 ##################### PROGRESSBAR
 
-progreSh() {
-	LR='\033[1;31m'
-	LG='\033[1;32m'
-	LY='\033[1;33m'
-	LC='\033[1;36m'
-	LW='\033[1;37m'
-	NC='\033[0m'
-	if [ "${1}" = "0" ]; then TME=$(date +"%s"); fi
-	SEC=$(printf "%04d\n" $(($(date +"%s") - ${TME})))
-	SEC="$SEC sec"
-	PRC=$(printf "%.0f" ${1})
-	SHW=$(printf "%3d\n" ${PRC})
-	LNE=$(printf "%.0f" $((${PRC} / 2)))
-	LRR=$(printf "%.0f" $((${PRC} / 2 - 12)))
-	if [ ${LRR} -le 0 ]; then LRR=0; fi
-	LYY=$(printf "%.0f" $((${PRC} / 2 - 24)))
-	if [ ${LYY} -le 0 ]; then LYY=0; fi
-	LCC=$(printf "%.0f" $((${PRC} / 2 - 36)))
-	if [ ${LCC} -le 0 ]; then LCC=0; fi
-	LGG=$(printf "%.0f" $((${PRC} / 2 - 48)))
-	if [ ${LGG} -le 0 ]; then LGG=0; fi
-	LRR_=""
-	LYY_=""
-	LCC_=""
-	LGG_=""
-	for ((i = 1; i <= 13; i++)); do
-		DOTS=""
-		for ((ii = ${i}; ii < 13; ii++)); do DOTS="${DOTS}."; done
-		if [ ${i} -le ${LNE} ]; then LRR_="${LRR_}#"; else LRR_="${LRR_}."; fi
-		echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${DOTS}${LY}............${LC}............${LG}............ ${SHW}%${NC}\r"
-		if [ ${LNE} -ge 1 ]; then sleep .05; fi
+function progress_bar() {
+	bar=""
+	total=10
+	[[ -z $1 ]] && input=0 || input=${1}
+	x="##"
+	for i in $(seq 1 10); do
+		if [ $i -le $input ]; then
+			bar=$bar$x
+		else
+			bar="$bar  "
+		fi
 	done
-	for ((i = 14; i <= 25; i++)); do
-		DOTS=""
-		for ((ii = ${i}; ii < 25; ii++)); do DOTS="${DOTS}."; done
-		if [ ${i} -le ${LNE} ]; then LYY_="${LYY_}#"; else LYY_="${LYY_}."; fi
-		echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${DOTS}${LC}............${LG}............ ${SHW}%${NC}\r"
-		if [ ${LNE} -ge 14 ]; then sleep .05; fi
-	done
-	for ((i = 26; i <= 37; i++)); do
-		DOTS=""
-		for ((ii = ${i}; ii < 37; ii++)); do DOTS="${DOTS}."; done
-		if [ ${i} -le ${LNE} ]; then LCC_="${LCC_}#"; else LCC_="${LCC_}."; fi
-		echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${DOTS}${LG}............ ${SHW}%${NC}\r"
-		if [ ${LNE} -ge 26 ]; then sleep .05; fi
-	done
-	for ((i = 38; i <= 49; i++)); do
-		DOTS=""
-		for ((ii = ${i}; ii < 49; ii++)); do DOTS="${DOTS}."; done
-		if [ ${i} -le ${LNE} ]; then LGG_="${LGG_}#"; else LGG_="${LGG_}."; fi
-		echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${LG}${LGG_}${DOTS} ${SHW}%${NC}\r"
-		if [ ${LNE} -ge 38 ]; then sleep .05; fi
-	done
-}
+	#pct=$((200*$input/$total % 2 + 100*$input/$total))
+	pct=$(($input * 10))
+	echo -ne "Progress : [ ${bar} ] (${pct}%) \r"
+	sleep 1
+	if [ $input -eq 10 ]; then
+		echo -ne '\n'
+	fi
 
+}
 ################################# CASES
 
 case $SYSTEM_OS in
@@ -174,24 +140,19 @@ function createEditor() {
 
 ################################# PROCESS
 
-printf "\n\n\n\n\n\n\n\n\n\n"
-progreSh 0
-progreSh 10
+
+progress_bar 1
 removeInstalledNvim
-progreSh 20
-progreSh 30
+progress_bar 2
 removeInstalledLvim
-progreSh 40
-progreSh 50
+progress_bar 3
 install
-progreSh 60
-progreSh 70
+progress_bar 5
 createNvim
-progreSh 80
-progreSh 90
+progress_bar 8
 createEditor
-progreSh 100
-printf "\n\n\n\n\n\n\n\n\n\n"
+progress_bar 10
+
 
 echo ""
 
